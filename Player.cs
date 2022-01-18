4e53998c9912119112e20297334e8910a3dacf60
Jorge Ponce <96438919+Lomalort@users.noreply.github.com>
2022-01-18 09:58:39 -0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,447 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody2D rb;
+	Vector2 movimiento;
+
+	public float velocidad;
+
+	public ParticleSystem orbeContacto;
+	public ParticleSystem badOrbeContact;
+	public ParticleSystem velocidadActivada;
+	public ParticleSystem orbeMalo;
+	public ParticleSystem particleGameOver;
+	public ParticleSystem perderOrbes;
+	public ParticleSystem SlowMoAct;
+
+
+	public Transform luzPlayer;
+
+	public AudioSource audioS;
+
+	public float pitchValue = 1f;
+
+	bool juegoATerminado = false;
+
+	//Texto UI
+	[SerializeField]
+	Text coinCounter;
+	//HiScore Ui
+	[SerializeField]
+	Text orbesMaximos;
+	[SerializeField]
+	Text orbesSumadosText;
+
+	[SerializeField]
+	GameObject coinMagnet;
+
+	//Almacenar los coins
+	int coinsNumber;
+	//Almacenar los orbes obtenidos
+	int orbesObtenidos;
+	int orbesSumados;
+
+	//Detener el player cuando toca un BadOrbe
+	bool _aturdido;
+
+	//Detener el player cuando muere
+	bool _gameOver;
+	
+	private Animator anim;
+
+	public AudioSource orbeSound;
+	
+    
+    // Variable para demorar el reinicio del nivel
+    public float restartDelay = 5f;
+
+	private int valorAleatorio;
+
+	private float? _tiempoInicioContadorMonedas;
+
+	private float _tiempoMaxMonedas = 1f;
+
+	public Image imagenOrbe;
+
+	//Sensibilidad en las flechas del Joystick
+	public float sensitivity = 3f;
+	
+	float smoothx;
+	float smoothy;
+
+	public bool extraDesbloqueado_1, extraDesbloqueado_2, extraDesbloqueado_3, extraDesbloqueado_4, extraDesbloqueado_5, extraDesbloqueado_6, extraDesbloqueado_7, extraDesbloqueado_8;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		orbesObtenidos = PlayerPrefs.GetInt("orbesObtenidos");
+
+		// Create a temporary reference to the current scene.
+		Scene currentScene = SceneManager.GetActiveScene();
+
+		// Retrieve the name of this scene.
+		string sceneName = currentScene.name;
+
+		if (sceneName == "Nivel_1")
+		{
+			orbesObtenidos = 0;
+		}
+		
+	}
+
+	float SmoothX()
+	{
+		float target = Input.GetAxis("Horizontal");
+		smoothx = Mathf.MoveTowards(smoothx,
+				   target, sensitivity * Time.deltaTime);		
+		return smoothx;
+	}
+	float SmoothY()
+	{
+		float target = Input.GetAxis("Vertical");
+		smoothy = Mathf.MoveTowards(smoothy,
+				   target, sensitivity * Time.deltaTime);		
+		return smoothy;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{	
+		
+		movimiento.x = SmoothX();
+		movimiento.y = SmoothY();
+
+		//Animacion del movimiento
+		anim.SetFloat("Horizontal", movimiento.x);
+		anim.SetFloat("Vertical", movimiento.y);
+
+		//Transformar la variable numerica a texto
+		coinCounter.text = coinsNumber.ToString();
+		orbesMaximos.text = orbesObtenidos.ToString();
+		orbesSumadosText.text = orbesSumados.ToString();
+
+        if (coinsNumber == 0)
+        {
+			coinCounter.color = Color.grey;
+			imagenOrbe.color = Color.gray;
+		}
+		
+			
+
+		coinMagnet.transform.position = new Vector2 (transform.position.x, transform.position.y);		
+		
+	}
+    private void FixedUpdate()
+    {
+		rb.velocity = movimiento * velocidad;
+
+		//Frenar al player
+		if (_aturdido)
+        {
+			rb.velocity = Vector2.zero;
+
+            _aturdido = true;
+
+        }
+
+		//Frenar al player
+        if (_gameOver)
+        {
+			rb.velocity = Vector2.zero;
+
+			_gameOver = true;
+		}
+        
+
+	}
+    void LateUpdate()
+    {
+		//Frenar el movimiento del player el tiempo que la animacion dure
+        if (anim.GetCurrentAnimatorStateInfo(0).IsTag("Aturdido"))
+        {
+            _aturdido = true;
+        }
+        else
+        {
+            _aturdido = false;
+        }
+
+		//Frenar el movimiento del player al morir segun el tiempo que la animacion dure
+		if (anim.GetCurrentAnimatorStateInfo(0).IsTag("GameOver"))
+		{
+			_gameOver = true;
+		}
+		else
+		{
+			_gameOver = false;
+		}
+	}
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag.Equals ("Coin")) 
+		{
+			if (_tiempoInicioContadorMonedas is null)
+			{
+				_tiempoInicioContadorMonedas = Time.time;
+			}		
+
+			//Instanciar sonido de Orbe
+			orbeSound.Play();
+
+			//FindObjectOfType<AudioManager>().Play("Orbe");
+
+			//Particulas cuando tocas el Orbe
+			LargarParticulas();			
+
+			//Destruir Objeto que tocas
+			Destroy (col.gameObject);
+			
+			//Sumar Orbes
+			coinsNumber += 1;
+			coinCounter.color = Color.white;
+			imagenOrbe.color = Color.white;
+
+			if (Time.time - _tiempoInicioContadorMonedas < _tiempoMaxMonedas)
+            {
+				//Subir el pitch
+				orbeSound.pitch = (pitchValue += 0.1f);
+            }
+            else
+            {
+                //Colocar el pitch en 1
+                orbeSound.pitch = (pitchValue = 1);
+
+                //...y colocar el tiempo Inicio contador en nulo
+                _tiempoInicioContadorMonedas = null;
+            }
+        }
+
+
+        if (col.gameObject.tag.Equals("BadOrbe"))
+		{
+			if(coinsNumber == 0)
+            {
+				anim.SetTrigger("GameOver");
+				GameOverPar();
+				Electricidad();
+				FindObjectOfType<AudioManager>().Play("BadOrbeDeath");				
+			}
+            //Instanciar sonido de BadOrbe
+            //Instantiate(badOrbeSound);
+            FindObjectOfType<AudioManager>().Play("BadOrbe");
+
+
+            //Instanciar particulas de BadOrbe
+            Electricidad();
+
+			//Frenar movimiento del player
+			_aturdido = true;
+
+			//Activar animacion Aturdido
+			anim.SetTrigger("Aturdido");
+
+			//Play particulas
+			badOrbeContact.Play();
+
+			PerderOrbesBuenos();
+
+			//StartCoroutine("VisualFeedback");
+
+			//Destruir el objeto que colicionas
+			Destroy(col.gameObject);
+
+			//Restar orbes
+			coinsNumber -= 10;
+
+			//Condicion para que los orbes no pasen de 0
+            if (coinsNumber < 0)
+            {
+				coinsNumber = 0;				
+			}
+
+		}
+
+
+		
+		//Condicion para Matar al Player
+		if (col.gameObject.tag.Equals("Fluid"))
+        {
+			//Animacion para frenar el personaje
+			anim.SetTrigger("GameOver");
+
+			FindObjectOfType<AudioManager>().Play("PlayerDeath");
+
+			FindObjectOfType<AudioManager>().Stop("BadOrbeDeath");
+
+			audioS.Stop();
+
+			//Fin del juego
+			EndGame();
+        }
+		if (col.gameObject.tag.Equals("PowerUp"))
+		{
+			valorAleatorio = Random.Range(0,2);
+			if(valorAleatorio == 0)
+            {
+				AumentoVelocidad();
+				StartCoroutine(aumentarVelocidad());				
+				Destroy(col.gameObject);
+			}
+			else if(valorAleatorio == 1)
+            {
+				AumentoSlowMo();
+				StartCoroutine(disminuirVelocidad());
+				Destroy(col.gameObject);
+			}
+						
+		}
+		if (col.gameObject.tag.Equals("SumadorOrbes"))
+        {
+			orbesSumados = coinsNumber + orbesObtenidos;
+			PlayerPrefs.SetInt("orbesObtenidos", orbesSumados);
+			Scene currentScene = SceneManager.GetActiveScene();
+
+			// Retrieve the name of this scene.
+			string sceneName = currentScene.name;
+
+			if (sceneName == "Nivel_1")
+            {
+				if (coinsNumber > 20)
+				{
+					extraDesbloqueado_1 = true;
+					PlayerPrefs.SetInt("Extralevel1", extraDesbloqueado_1 ? 1 : 0);
+				}
+			}
+			else if (sceneName == "Nivel_2")
+			{
+				if (coinsNumber > 20)
+				{
+					extraDesbloqueado_2 = true;
+					PlayerPrefs.SetInt("Extralevel2", extraDesbloqueado_2 ? 1 : 0);
+				}
+			}
+			else if (sceneName == "Nivel_3")
+			{
+				if (coinsNumber > 20)
+				{
+					extraDesbloqueado_3 = true;
+					PlayerPrefs.SetInt("Extralevel3", extraDesbloqueado_3 ? 1 : 0);
+				}
+			}
+			else if (sceneName == "Nivel_4")
+			{
+				if (coinsNumber > 20)
+				{
+					extraDesbloqueado_4 = true;
+					PlayerPrefs.SetInt("Extralevel4", extraDesbloqueado_4 ? 1 : 0);
+				}
+			}
+			else if (sceneName == "Nivel_5")
+			{
+				if (coinsNumber > 20)
+				{
+					extraDesbloqueado_5 = true;
+					PlayerPrefs.SetInt("Extralevel5", extraDesbloqueado_5 ? 1 : 0);
+				}
+			}
+			else if (sceneName == "Nivel_6")
+			{
+				if (coinsNumber > 20)
+				{
+					extraDesbloqueado_6 = true;
+					PlayerPrefs.SetInt("Extralevel6", extraDesbloqueado_6 ? 1 : 0);
+				}
+			}
+			else if (sceneName == "Nivel_7")
+			{
+				if (coinsNumber > 20)
+				{
+					extraDesbloqueado_7 = true;
+					PlayerPrefs.SetInt("Extralevel7", extraDesbloqueado_7 ? 1 : 0);
+
+                    if (orbesObtenidos > 1000)
+                    {
+						extraDesbloqueado_8 = true;
+						PlayerPrefs.SetInt("ExtralevelSpiriat", extraDesbloqueado_8 ? 1 : 0);
+					}
+				}
+			}
+
+		}
+
+
+	}
+
+	void LargarParticulas()
+	{
+		orbeContacto.Play();
+	}
+	void AumentoVelocidad()
+    {
+		velocidadActivada.Play();
+
+	}
+	void Electricidad()
+    {
+		orbeMalo.Play();
+    }
+	void GameOverPar()
+    {
+		particleGameOver.Play();
+    }
+	void PerderOrbesBuenos()
+    {
+		perderOrbes.Play();
+    }
+	void AumentoSlowMo()
+    {
+		SlowMoAct.Play();
+    }
+
+	public void EndGame()
+    {
+		if(juegoATerminado == false)
+        {
+			juegoATerminado = true;
+			Debug.Log("GAME OVER");
+			Invoke("Restart", restartDelay);			
+        }
+    }
+	void Restart()
+    {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+	private IEnumerator aumentarVelocidad()
+    {		
+		velocidad = 30f;
+		audioS.pitch = (pitchValue = 1.05f);
+		yield return new WaitForSeconds(9f);
+		velocidad = 15f;
+		audioS.pitch = (pitchValue = 1f);
+	}
+	
+	private IEnumerator disminuirVelocidad()
+    {
+		audioS.pitch = (pitchValue = 0.95f);
+		velocidad = 3f;
+		yield return new WaitForSeconds(9f);
+		velocidad = 15f;
+		audioS.pitch = (pitchValue = 1f);
+	}
+
+	
+	
+}
---
+++ PlayerController.cs
@@ -0,0 +1,299 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float longIdleTime = 5f;
+	public float speed = 2.5f;
+	public float jumpForce = 2.5f;
+
+	public Transform groundCheck;
+	public LayerMask groundLayer;
+	public float groundCheckRadius;
+	public GameObject dustEffect;
+	private bool _salidaPolvo;
+	public ParticleSystem polvo;
+
+	// References
+	private Rigidbody2D _rigidbody;
+	public Animator _animator;
+	public static PlayerController instance;
+
+	// Long Idle
+	private float _longIdleTimer;
+
+	// Movement
+	private Vector2 movement;
+	private bool _facingRight = true;
+	private bool _isGrounded;
+	private bool _crouch;
+	private bool _run;
+	
+
+	// Attack
+	private bool _isAttacking;
+	private bool _jump;
+	private bool _dobleJump;
+	private bool _jumpAttack;
+	//private SpriteRenderer _renderer;
+	public GameObject espadaBrillo;
+
+	//Coyote Time
+	float tiempoEnElAire;
+
+	//Input Buffer
+	//Queue<KeyCode> inputBuffer;
+
+
+	
+
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody2D>();
+		_animator = GetComponent<Animator>();
+		instance = this;
+		//_renderer = GetComponent<SpriteRenderer>();
+		
+	}
+
+	void Start()
+    {
+		polvo = GetComponentInChildren<ParticleSystem>();
+		_animator = GetComponent<Animator>();
+		espadaBrillo = GetComponentInChildren<GameObject>();	
+	}
+
+    void Update()
+    {		
+		if (_isAttacking == false) 
+		{
+            // Movement
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            movement = new Vector2(horizontalInput, 0f);			
+
+
+
+			// Flip character
+			if (horizontalInput < 0f && _facingRight == true) 
+			{				
+				Flip();
+			} 
+			else if (horizontalInput > 0f && _facingRight == false) 
+			{
+				Flip();
+			}
+		}
+
+
+
+        // Is Grounded?
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        //RaycastHit2D raycastSuelo = Physics2D.Raycast(transform.position, Vector2.down, 0.25f, groundLayer);
+
+        //Largando Polvo
+
+        if (_isGrounded == true)
+        {
+			tiempoEnElAire = 0;
+
+			if (_salidaPolvo == true)
+            {
+				polvo.Play();
+				_salidaPolvo = false;
+            }
+
+        }
+        else
+        {
+			tiempoEnElAire += Time.deltaTime;
+			_salidaPolvo = true;
+        }
+
+
+        // Is Jumping?
+        if (Input.GetButtonDown("Jump") && _isAttacking == false) 
+		{
+            if (_isGrounded || tiempoEnElAire < 0.25f)
+            {
+				
+				CrearPolvo();
+
+				_rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+				_isAttacking = false;
+				_dobleJump = true;
+				_jumpAttack = true;
+				
+			}
+           
+
+			else if (_dobleJump)
+            {
+				_rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+				_dobleJump = false;
+				_jumpAttack = true;
+
+				StartCoroutine("VisualFeedback");			
+                				
+			}			
+
+		}
+
+		// Wanna Run?
+		if (Input.GetButtonDown("Fire2") && _isGrounded == true && _isAttacking == false && _run == false)
+		{
+			movement = Vector2.zero;
+			_rigidbody.velocity = Vector2.zero;
+			_animator.SetTrigger("ToRun");
+			speed = 7f;
+			_run = true;
+			polvo.Play();
+		}
+		if (Input.GetButtonUp("Fire2") && _isGrounded == true && _isAttacking == false && _run == true)
+		{
+			movement = Vector2.zero;
+			_rigidbody.velocity = Vector2.zero;
+			_animator.SetTrigger("ToWalk");
+			speed = 4f;
+			_run = false;
+		}
+		// Wanna Attack?
+		if (Input.GetButtonDown("Fire1") && _isGrounded == true && _isAttacking == false) 
+		{
+			
+			movement = Vector2.zero;
+			_rigidbody.velocity = Vector2.zero;
+			_animator.SetTrigger("0");
+            _isAttacking = true;
+
+        }
+
+		if (Input.GetButtonDown("Fire1") && _isGrounded == false && _jumpAttack == true && _isAttacking == false)
+		{
+			
+			movement = Vector2.zero;
+			_rigidbody.velocity = Vector2.zero;
+			_animator.SetTrigger("JumpAttack");
+			_jumpAttack = false;
+			_isAttacking = true;
+
+		}
+
+		// Agacharce
+		if (Input.GetButtonDown("Crouch") && _isGrounded == true)
+        {
+			_crouch = true;			
+			_animator.SetBool("Crouch", true);
+			
+		}
+			
+		else if (Input.GetButtonUp("Crouch"))
+        {
+			_crouch = false;			
+			_animator.SetBool("Crouch", false);
+						
+		}
+
+		// Atacar Agachado
+		if (_crouch == true && Input.GetButtonDown("Fire1"))
+        {
+			
+			_isAttacking = true;			
+			_animator.SetTrigger("CrouchAttack");
+
+        }
+			
+		
+	}
+
+	void FixedUpdate()
+	{
+		if (_isAttacking == false) 
+		{
+            float horizontalVelocity = movement.normalized.x * speed;
+            _rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y);
+			
+		}
+		if (_isAttacking == true)
+		{
+			movement = Vector2.zero;
+			_rigidbody.velocity = Vector2.zero;
+			
+		}
+		if (_crouch == true)
+        {
+			movement = Vector2.zero;
+			_rigidbody.velocity = Vector2.zero;
+		}
+	}
+	
+
+	void LateUpdate()
+	{
+		_animator.SetBool("Idle", movement == Vector2.zero);
+		_animator.SetBool("IsGrounded", _isGrounded);
+		_animator.SetFloat("VerticalVelocity", _rigidbody.velocity.y);
+
+		// Animator
+		
+		if (_animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")) {
+			_isAttacking = true;
+		} else {
+			_isAttacking = false;
+		}
+		
+
+		// Long Idle
+		if (_animator.GetCurrentAnimatorStateInfo(0).IsTag("Idle")) {
+			_longIdleTimer += Time.deltaTime;
+
+			if (_longIdleTimer >= longIdleTime) {
+				_animator.SetTrigger("LongIdle");
+			}
+		} else {
+			_longIdleTimer = 0f;
+		}
+	}
+
+	private void Flip()
+	{
+		if (_isGrounded == true)
+		{
+			CrearPolvo();
+		}
+		_facingRight = !_facingRight;
+		float localScaleX = transform.localScale.x;
+		localScaleX = localScaleX * -1f;
+		transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+
+	}
+	private IEnumerator VisualFeedback()
+	{
+		espadaBrillo.SetActive(true);
+		
+		//_renderer.color = Color.blue;
+
+		yield return new WaitForSeconds(0.5f);
+
+		espadaBrillo.SetActive(false);
+
+		//_renderer.color = Color.white;
+	}
+	private void CrearPolvo()
+    {
+        
+		polvo.Play();
+		
+	}
+
+}
+
